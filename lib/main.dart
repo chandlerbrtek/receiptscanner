d58a05197269b93461214a29928c17b93fe66d78
Luke Moorhous <lukemoorhous@gmail.com>
2019-06-13 19:11:07 -0500

    #6 - Updated title of Home Page to "Receipt Scanner"

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Receipt Scanner'),
     );
   }
 }
