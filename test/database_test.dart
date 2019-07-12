13fed7f445d3db52d9b2eb92bbf50f811b642c15
Luke Moorhous <lukemoorhous@gmail.com>
2019-07-12 15:48:15 -0500

    #34 - Added basic unit tests.

--- test/database_test.dart
+++ test/database_test.dart
@@ -1,18 +1,61 @@
-import 'dart:math';
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:receipt/data/data-api.dart';
 import 'package:receipt/data/receipt.dart';
 
-int index = Random(TimeOfDay.now().minute).nextInt(100000) + 10000;
+
 
 void main() {
   test('Add Receipt', () {
-      int date = 1001, total = 100;
-      print('Adding Receipt:\nid:\t$index\nreceiptDate:\t$date\ntotal:\t$total');
-      ReceiptAPI.add(new Receipt(id: index, receiptDate: date, total: total));
+    Receipt receipt = new Receipt(receiptDate: 1001, total: 100);
+      ReceiptAPI.add(receipt);
+    }
+  );
+
+  test('Retrieve Receipt', () {
+      Receipt receipt = new Receipt(receiptDate: 1021, total: 100);
+      int index = ReceiptAPI.add(receipt);
+      Receipt resp = ReceiptAPI.get(index);
+      expect(resp.total, receipt.total);
+      expect(resp.receiptDate, receipt.receiptDate);
+    }
+  );
+
+  test('Update Receipt', () {
+    Receipt receipt = new Receipt(receiptDate: 13001, total: 100);
+      int index = ReceiptAPI.add(receipt);
+      
+      Receipt update = new Receipt(id: index, receiptDate: 111101, total: 357);
+
+      ReceiptAPI.update(index, update);
+      
+      Receipt resp = ReceiptAPI.get(index);
+      
+      expect(resp.total, update.total);
+      expect(resp.receiptDate, update.receiptDate);
+      
+      receipt = update;
+    }
+  );
+
+  test('Find Receipt', () {
+      Receipt receipt = new Receipt(receiptDate: 34002, total: 1369);
+      int index = ReceiptAPI.add(receipt);
+
+      List<Receipt> results = ReceiptAPI.find(receipt);
+      Receipt resp = results.removeLast();
+
+      expect(resp.id, index);
+      expect(resp.total, receipt.total);
+      expect(resp.receiptDate, receipt.receiptDate);
+    }
+  );
+
+  test('Delete Receipt', () {
+      Receipt receipt = new Receipt(receiptDate: 1221, total: 100);
+      int index = ReceiptAPI.add(receipt);
+      ReceiptAPI.delete(index);
+      expect(ReceiptAPI.get(index), new List<Receipt>());
     }
   );
 }
